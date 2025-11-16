21a8dbd4e459aa19ea95a0eb4343ce3ef1aced93
omarakamal <omaraakamal@gmail.com>
2025-11-16 18:52:34 +0300

    update

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
-console.log('==========')
-console.log('Good Morning Mohammad ')
-console.log('==========')
+// console.log('==========')
+// console.log('Good Morning Mohammad ')
+// console.log('==========')
 
 
 
-console.log('==========')
-console.log('Good Morning Mohammad ')
-console.log('==========')
+// console.log('==========')
+// console.log('Good Morning Mohammad ')
+// console.log('==========')
 
 
-// 
+// 2 parts of working with functions
+
+// 1. declaring a function
+
+
+function greetMohammad(){
+    console.log('==========')
+    console.log('Good Morning Mohammad ')
+    console.log('==========')
+}
+
+// 2. calling a function
+// greetMohammad()
+// greetMohammad()
+
+
+
+
+function greetStudent(){
+    console.log('==========')
+    console.log('Good Morning Mohammad ')
+    console.log('==========')
+}
+
+
